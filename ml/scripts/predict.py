77d19044c1e6ca4f116da705c71b8d161ababf9c
Fuchtheviper <f.sukrit.b@gmail.com>
2025-01-19 14:39:31 +0700

    edit model to best_model

--- ml/scripts/predict.py
+++ ml/scripts/predict.py
@@ -1,7 +1,6 @@
 import tensorflow as tf
 import numpy as np
 
-def predict_revenue(model_path, X_test):
-    model = tf.keras.models.load_model(model_path)
-    predictions = model.predict(X_test)
+def predict_revenue(best_model, X_test):
+    predictions = best_model.predict(X_test)
     return predictions
