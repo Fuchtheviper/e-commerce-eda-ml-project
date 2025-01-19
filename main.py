798462818c693c6edfb5e140b15baed239d44b75
Fuchtheviper <f.sukrit.b@gmail.com>
2025-01-19 14:40:29 +0700

    change from model path to best model

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from ml.scripts.data_preprocessing import preprocess_data, create_sequence
 from ml.scripts.train_model import train_model
 from ml.scripts.predict import predict_revenue
 from ml.scripts.evaluate_model import evaluate_model
+from ml.scripts.hyperparameter_tuning import perform_hyperparameter_tuning, train_best_model
 import matplotlib.pyplot as plt
 import os
 
@@ -17,13 +18,25 @@ if __name__ == "__main__":
 
 
     # model
-    model_path = 'ml/models/lstm_revenue_prediction_re1.keras'
+    model_path = 'ml/models/lstm_revenue_prediction_re2.keras'
+    output_dir = 'ml/models/tuning_results'
+    num_features = X_train_seq.shape[2]
 
     input_shape = (X_train_seq.shape[1], X_train_seq.shape[2])
     print(f"LSTM Input Shape: {input_shape}")
 
+    tuner = perform_hyperparameter_tuning(
+    X_train_seq, y_train_seq,
+    X_val_seq, y_val_seq,
+    lookback, num_features,
+    output_dir
+)
+
+# Train the best model
+    best_model = train_best_model(tuner)
+
     # Train model
-    history = train_model(X_train_seq, y_train_seq, X_val_seq, y_val_seq, model_path)
+    history = train_model(X_train_seq, y_train_seq, X_val_seq, y_val_seq, best_model)
     # Plot training and validation loss
     plt.figure(figsize=(10, 6))
     plt.plot(history.history['loss'], label='Training Loss')
@@ -36,7 +49,7 @@ if __name__ == "__main__":
     plt.show()
 
     # Predict
-    y_pred = predict_revenue(model_path, X_test_seq)
+    y_pred = predict_revenue(best_model, X_test_seq)
     # Plot actual vs predicted
     plt.figure(figsize=(10, 6))
     plt.plot(range(len(y_test_seq)), y_test_seq, label='Actual Revenue', color='blue')
